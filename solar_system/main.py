f20dd671433dfc8a0dd5c6ef12fbabf2d99c4993
Ruslan <rus.babaef@yandex.ru>
2021-11-13 01:25:22 +0300

    update 1.3

--- solar_system/main.py
+++ solar_system/main.py
@@ -1,12 +1,13 @@
 import sys
-import sqlite3
 
-from solar_objects import SOLAROBJECTS
+from solar_objects import SOLAR_OBJECTS, CLICKED_SOLAR_OBJECT
 
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QPropertyAnimation, QPointF
+from PyQt5.QtGui import QPixmap, QPainterPath, QPainter
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5 import uic
 from main_window import Ui_MainWindow
+from solar_object_info import Ui_SolarObjectInfo
 
 
 class ModelSolarSystem(QMainWindow, Ui_MainWindow):
@@ -14,27 +15,84 @@ class ModelSolarSystem(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.initButtons()
+        self.initPaths()
+        self.initAnimations()
+        self.repaint()
 
     def initButtons(self):
+        global SOLAR_OBJECTS
         solar_views = [self.sun, self.mercury, self.venerus, self.earth, self.mars,
                        self.jupiter, self.saturn, self.uran, self.neptun, self.pluton]
-        for i in range(len(SOLAROBJECTS)):
-            SOLAROBJECTS[i].view = solar_views[i]
-            SOLAROBJECTS[i].view.clicked.connect(self.show_description)
+        for i in range(len(SOLAR_OBJECTS)):
+            SOLAR_OBJECTS[i].objName = solar_views[i].objectName()
+            SOLAR_OBJECTS[i].view = solar_views[i]
+            SOLAR_OBJECTS[i].view.clicked.connect(self.show_info)
+        self.up_button.clicked.connect(self.start)
+        self.down_button.clicked.connect(self.start)
+        self.stop_button.clicked.connect(self.stop)
+        self.reset_button.clicked.connect(self.reset)
 
-    def show_description(self):
-        self.space_object = SpaceObject(self,)
+    def initPaths(self):
+        self.paths, k, r = [], 0, 1
+        for i in range(1, 10):
+            path = QPainterPath()
+            path.addEllipse(480 - k, 330 - k, int(r * 140), int(r * 140))
+            k += 35
+            r += 0.5
+            self.paths.append(path)
+
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QPainter.Antialiasing)
+        for p in self.paths:
+            qp.drawPath(p)
+        qp.end()
+
+    def initAnimations(self):
+        self.animations = []
+        for i in range(9):
+            anim = QPropertyAnimation(SOLAR_OBJECTS[i + 1].view, b'pos')
+            anim.setDuration(10000 * (i + 1))
+            values = [p / 100 for p in range(0, 101)]
+            for j in values:
+                anim.setKeyValueAt(j, self.paths[i].pointAtPercent(j))
+            anim.setEndValue(QPointF(SOLAR_OBJECTS[i + 1].view.x(), SOLAR_OBJECTS[i].view.y()))
+            self.animations.append(anim)
+
+    def start(self):
+        for a in self.animations:
+            a.start()
+
+    def stop(self):
+        for a in self.animations:
+            a.stop()
+
+    def reset(self):
+        pass
+
+    def show_info(self):
+        global CLICKED_SOLAR_OBJECT
+        view = self.sender().objectName()
+        for i in SOLAR_OBJECTS:
+            if i.objName == view:
+                CLICKED_SOLAR_OBJECT = i
+                break
+        self.space_object = SolarObjectInfo(self)
         self.space_object.show()
 
 
-class SpaceObject(QWidget):
+class SolarObjectInfo(QWidget, Ui_SolarObjectInfo):
     def __init__(self, *args):
         super().__init__()
-        uic.loadUi('space_object.ui', self)
-        self.setWindowTitle(SOLAROBJECTS[3].name)
-        self.spaceObjectName.setText(SOLAROBJECTS[3].name)
-        self.description.setText(SOLAROBJECTS[3].to_description())
-
+        self.setupUi(self)
+        self.setWindowTitle(CLICKED_SOLAR_OBJECT.name)
+        self.solar_object_name.setText(CLICKED_SOLAR_OBJECT.name)
+        self.main_info.setText(CLICKED_SOLAR_OBJECT.to_main_info())
+        self.other_info.setText(CLICKED_SOLAR_OBJECT.to_other_info())
+        pix = QPixmap(CLICKED_SOLAR_OBJECT.image)
+        self.img.setPixmap(pix)
 
 
 if __name__ == '__main__':
---
+++ solar_system/solar_objects.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+from PyQt5.QtCore import QPointF, pyqtProperty
+from PyQt5.QtWidgets import QPushButton
+
+DATA = sqlite3.connect('solar_objects_db.db').cursor().execute("""SELECT * FROM SolarObjects""")
+DATA = [list(i) for i in DATA]
+SOLAR_IMAGES = ['images/sun.png', 'images/mercury.png', 'images/venerus.png', 'images/earth.png',
+                'images/mars.png', 'images/jupiter.png', 'images/saturn.png', 'images/uran.png',
+                'images/neptun.png', 'images/pluton.png']
+SOLAR_OBJECTS, CLICKED_SOLAR_OBJECT = [], None
+
+
+class SolarObject:
+
+    def __init__(self, objName, view, image, *args):
+        self.objName = objName
+        self.view = view
+        self.image = image
+        self.info = args[0]
+        self.name = self.info[1]
+
+    def to_main_info(self):
+        main_data = [i for i in self.info[2:11]]
+        return f'Тип: {main_data[0]}\n\nРасстояние от Солнца: {main_data[1]} а.е.\n\n' \
+               f'Период обращения (в земных годах): {main_data[2]}\n\nЭксцентриситет: {main_data[3]}\n\n' \
+               f'Орбитальная скорость: {main_data[4]} км/с\n\nПлотность: {main_data[5]}x10^3 кг/м^3\n\n' \
+               f'Ускорение свободного падения: {main_data[6]} м/с^2\n\n' \
+               f'Масса (в массах Земли): {main_data[7]}\n\nКоличество спутников: {main_data[8]}\n\n' \
+
+    def to_other_info(self):
+        d, atmosphere, description = 0, [], []
+        for i in range(1, len(self.info[11])):
+            if len(self.info[11]) < 124:
+                atmosphere.append(self.info[11])
+                break
+            if i % 124 == 0:
+                atmosphere.append(self.info[11][d:i + 1])
+                d = i + 1
+                if len(self.info[11][i + 1:]) < 124:
+                    atmosphere.append(self.info[11][i + 1:])
+                    break
+        for i in range(1, len(self.info[12])):
+            if len(self.info[12]) < 124:
+                description.append(self.info[12].strip())
+                break
+            if i % 124 == 0:
+                description.append(self.info[12][d:i + 1].strip())
+                d = i + 1
+                if len(self.info[12][i + 1:]) < 124:
+                    description.append(self.info[12][i + 1:].strip())
+                    break
+        atmosphere, description = '\n'.join(atmosphere), '\n'.join(description)
+        return f'Состав атмосферы:\n{atmosphere}\n\nОписание:\n{description}'
+
+    def _set_pos(self, pos):
+
+        self.view.move(pos.x() - self.w / 2, pos.y() - self.h / 2)
+
+    pos = pyqtProperty(QPointF, fset=_set_pos)
+
+
+for i in range(10):
+    SOLAR_OBJECTS.append(SolarObject(None, None, SOLAR_IMAGES[i], DATA[i]))
